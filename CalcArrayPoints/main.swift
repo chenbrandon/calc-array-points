67fbeb9234c33128eeb77fa0431511f9b4c0f27b
Brandon Chen <chenb777@uw.edu>
2017-04-05 22:39:49 -0700

    Initial Commit

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by Brandon Chen on 4/5/17.
+//  Copyright © 2017 Brandon Chen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
